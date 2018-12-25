7d0f3d7d3313ac94027250cc8daeb6e5860b0c3f
Dimon <vadimvalec@gmail.com>
2018-12-25 13:13:47 +0200

    2 commit

--- 555.js
+++ 555.js
@@ -6,5 +6,14 @@ console.log(second);
 let answer = + prompt("Ведите число: ", "");
 console.log("a" + "Boob");
 console.log(answer+25);
-console.log(answer+33);
 console.log(answer+43);
+console.log("555");
+
+let arr = [1, 12, 31, 4, 5, 2];
+
+arr.sort(compareNum);
+function compareNum (a, b){
+    return a-b;
+}
+
+console.log(arr);
